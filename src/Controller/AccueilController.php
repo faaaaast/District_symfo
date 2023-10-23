ed48761aacdf933304a28af430f7cfdd9d20e6e3
duval theo <theoduval76@icloud.com>
2023-10-23 16:54:47 +0200

    modif

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtistRepository;
+use App\Repository\DiscRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccueilController extends AbstractController
 {
+    
+
+    private $artistRepo;
+    private $discRepo;
+    private $em;
+
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, EntityManagerInterface $em)
+    {
+        $this->artistRepo = $artistRepo;
+        $this->discRepo = $discRepo;
+        $this->em = $em;
+
+    }
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+        $artistes = $this->artistRepo->getSomeArtists("Damso");
+
+        dd($artistes); 
+
+     // ...    
+
     }
+
+    
+
 }
---
+++ src/Controller/MailerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MailerController extends AbstractController
+{
+    #[Route('/email', name: 'app_mailer')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        try {
+            $email = (new Email())
+                ->from('theo76@example.com')
+                ->to('test@example.com')
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>Mail envoyé avec succès !</p>'); 
+
+            $mailer->send($email);
+
+            return new Response('Email sent successfully');
+        } catch (\Exception $e) {
+            return new Response('Error sending email: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
